367cdddb8516c2384ce841b7ff5d1e2cf1192c1b
KristiyanP-03 <114045349+KristiyanP-03@users.noreply.github.com>
2023-08-11 21:39:28 +0300

    Project Groups are set

--- SoftUni Advanced - January 2023/Python WEB Framework/Regular Exam/project/project/settings.py
+++ SoftUni Advanced - January 2023/Python WEB Framework/Regular Exam/project/project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9=cgj#vbwn(op48=xwsi(a9j)5b!3&uhk19qt4&1zf8j!1@i*)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
