8e60c2738bcb08a875fc17cdb1e0db770c5b7a64
Kristiyan Pisev <114045349+KristiyanP-03@users.noreply.github.com>
2023-08-23 19:15:16 +0300

    I can do easy exrs and create .js files

---
+++ SoftUni Advanced - January 2023/JS Front-End/01. JS Syntax Fundamentals - Lab/JS Syntax Fundamentals - Lab.js
@@ -0,0 +1,42 @@
+//1. Multiply the Number by 2
+/* 
+function MultiplytheNumberbyTwo(num)
+{
+    result = num * 2
+    console.log(result)
+}
+
+MultiplytheNumberbyTwo(2)
+*/
+
+
+
+//2. Student Information
+/*
+function StudentInformation(student_name, student_age, student_grade)
+{ 
+    let string = `Name: ${student_name}, Age: ${student_age}, Grade: ${student_grade.toFixed(2)}`
+
+    console.log(string)
+}
+*/
+
+
+
+//3. Excellent grade
+/* 
+function ExcellentGrade(grade){
+    let answer = NaN;
+
+    if (grade >= 5.50)
+    {
+        answer = "Excellent";
+    }
+    else
+    {
+        answer = "Not excellent";
+    }
+
+    console.log(answer)
+} 
+*/
