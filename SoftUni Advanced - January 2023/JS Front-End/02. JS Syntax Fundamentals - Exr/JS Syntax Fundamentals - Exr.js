c055d602b1d2c2aef4bbdcac257f7f63917e6bf1
KristiyanP-03 <114045349+KristiyanP-03@users.noreply.github.com>
2023-09-11 21:27:30 +0300

    homework

---
+++ SoftUni Advanced - January 2023/JS Front-End/02. JS Syntax Fundamentals - Exr/JS Syntax Fundamentals - Exr.js
@@ -0,0 +1,116 @@
+//1. Ages
+/*
+function Ages(age){
+    let output = NaN;
+
+    if (0 <= age && age <= 2) {
+        output = "baby";
+    }
+    else if (3 <= age && age <= 13) {
+        output = "child";
+    }
+    else if (14 <= age && age <= 19) {
+        output = "teenager";
+    }
+    else if (20 <= age && age <= 65) {
+        output = "adult";
+    }
+    else if (66 <= age) {
+        output = "elder";
+    }
+    else{
+        output = "out of bounds";
+    }
+
+    console.log(output)
+}
+*/
+
+//2. Vacation
+/*
+function Vacation(group_of_people, type_of_the_group, day_of_the_week){
+    let price = 0;
+    let discont = 1;
+
+    switch (type_of_the_group) {
+
+        case "Students":
+            if (group_of_people >= 30) {
+                discont = 0.85
+            }
+
+            if (day_of_the_week === "Friday") {
+                let cost = 8.45;
+
+                price = (group_of_people * cost) * discont;
+            }
+            else if (day_of_the_week === "Saturday") {
+                let cost = 9.80;
+
+                price = (group_of_people * cost) * discont;
+            }
+            else if (day_of_the_week === "Sunday") {
+                let cost = 10.46;
+
+                price = (group_of_people * cost) * discont;
+            }
+            
+            break;
+
+
+        case "Business":
+            if (group_of_people >= 100) {
+                group_of_people = group_of_people - 10
+            }
+
+            if (day_of_the_week === "Friday") {
+                let cost = 10.90;
+
+                price = (group_of_people * cost) * discont;
+            }
+            else if (day_of_the_week === "Saturday") {
+                let cost = 15.60;
+
+                price = (group_of_people * cost) * discont;
+            }
+            else if (day_of_the_week === "Sunday") {
+                let cost = 16;
+
+                price = (group_of_people * cost) * discont;
+            }
+            break;
+
+
+        case "Regular":
+            if (group_of_people >= 10 && group_of_people <= 20) {
+                discont = 0.95
+            }
+
+            if (day_of_the_week === "Friday") {
+                let cost = 15;
+
+                price = (group_of_people * cost) * discont;
+            }
+            else if (day_of_the_week === "Saturday") {
+                let cost = 20;
+
+                price = (group_of_people * cost) * discont;
+            }
+            else if (day_of_the_week === "Sunday") {
+                let cost = 22.50;
+
+                price = (group_of_people * cost) * discont;
+            }
+            break;
+    
+
+
+        default:
+            break;
+    }
+
+
+    console.log(`Total price: ${price.toFixed(2)}`);
+    
+} 
+*/
