91f5de8710e82395d415afb503eac2567bcfe0e1
Kristiyan Pisev <kristiyanpisev@gmail.com>
2023-09-25 21:45:08 +0300

    JS OOP Exrs
    So much differences between JS OOP and Python OOP

---
+++ SoftUni Advanced - January 2023/JS Front-End/08. Objects and Classes - Exr/08. Objects and Classes - Exr.js
@@ -0,0 +1,19 @@
+//1. Employees
+/*
+function Employees(array){
+    for (let index = 0; index < array.length; index++) {
+        const object = {
+            employeeName: array[index],
+            personalNum: array[index].length,
+        };
+        console.log(`Name: ${object.employeeName} -- Personal Number: ${object.personalNum}`);
+    }
+}
+
+Employees([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+])
+*/
