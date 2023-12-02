4160528e6d68c44e2f593051d3044eafc37d6256
Kristiyan Pisev <114045349+KristiyanP-03@users.noreply.github.com>
2023-12-02 17:30:42 +0200

    Update 08. Objects and Classes - Exr.js

--- SoftUni Advanced - January 2023/JS Front-End/08. Objects and Classes - Exr/08. Objects and Classes - Exr.js
+++ SoftUni Advanced - January 2023/JS Front-End/08. Objects and Classes - Exr/08. Objects and Classes - Exr.js
@@ -19,3 +19,20 @@ Employees([
 */
 
 //2. Towns
+/*
+function TownsInfo(array) {
+    for (let index = 0; index < array.length; index++) {
+      let currentTownsInfo = array[index].split(" | ");
+  
+      let townModel = {
+        town: currentTownsInfo[0],
+        latitude: Number(currentTownsInfo[1]).toFixed(2), 
+        longitude: Number(currentTownsInfo[2]).toFixed(2)
+      };
+  
+      console.log(townModel);
+    }
+}
+
+TownsInfo(['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625'] );
+*/
