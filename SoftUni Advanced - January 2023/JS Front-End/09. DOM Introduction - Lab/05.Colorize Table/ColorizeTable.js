efbe5221383eac8d8ca476448c0a2af78b19e7af
Kristiyan Pisev <114045349+KristiyanP-03@users.noreply.github.com>
2023-12-09 20:50:14 +0200

    I've understood DOM

--- SoftUni Advanced - January 2023/JS Front-End/09. DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ SoftUni Advanced - January 2023/JS Front-End/09. DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+    const allRows = Array.from(document.querySelectorAll('td'));
+
+    for (let index = 0; index < allRows.length; index += 4) {
+        allRows[index].style.background = 'teal';
+        allRows[index + 1].style.background = 'teal';
+    }
 }
--- SoftUni Advanced - January 2023/JS Front-End/09. DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ SoftUni Advanced - January 2023/JS Front-End/09. DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
+    const prices = document.querySelectorAll("td:nth-child(2)"); 
 
+    let sum = 0;
+
+    for (let index = 0; index < prices.length; index++) {
+        sum += Number(prices[index].textContent);
+    }
+
+    document.getElementById("sum").textContent = sum.toFixed(2);
 }
