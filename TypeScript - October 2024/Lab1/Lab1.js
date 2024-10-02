d5c7c49c73f5abf2b37e84f6c54407e6d2cea4f9
Kristiyan Pisev <kristiyanpisev@gmail.com>
2024-10-02 20:50:43 +0300

    New course TypeScript started
    Gonna be a challenging one...

---
+++ TypeScript - October 2024/Lab1/Lab1.js
@@ -0,0 +1,12 @@
+// Exr. 1 -------------------------------------------------------------------------------------------------------------------
+function CalculateRectangleArea(a, b) {
+    return a * b;
+}
+var result = CalculateRectangleArea(5, 7);
+console.log(result);
+// to run use following command: 
+// tsc [filename].ts && node [filename].js
+// in our case: tsc Lab1.ts && node Lab1.js /// Not a valid statement !?!?!?
+// well do this:
+// tsc Lab1.ts
+// node Lab1.js
---
+++ TypeScript - October 2024/Lab1/Lab1.ts
@@ -0,0 +1,16 @@
+// Exr. 1 -------------------------------------------------------------------------------------------------------------------
+function CalculateRectangleArea(a: number, b: number){
+    return a * b;
+}
+
+const result: number = CalculateRectangleArea(5, 7);
+
+console.log(result);
+
+// to run use following command: 
+// tsc [filename].ts && node [filename].js
+// in our case: tsc Lab1.ts && node Lab1.js /// Not a valid statement !?!?!?
+
+// well do this:
+// tsc Lab1.ts
+// node Lab1.js
